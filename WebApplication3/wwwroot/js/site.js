04b7f25de3bcfbd1a312f298ab5e32f41d0459b3
wdragow <lbp1108@hotmail.com>
2020-01-30 23:38:23 -0300

    #ENC ajsutes layout

--- WebApplication3/wwwroot/js/site.js
+++ WebApplication3/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(window).resize(function () {
+    var width = $(window).width();
+
+    if (width < 730) {
+        $('#header').html('<nav class="links" id="menuheader" style="--items: 5;"><i style="font-size:56px;padding-right: 7px;padding-left: 20px;" class="material-icons">list</i></nav>').change();
+
+    } else {
+        $('#header').html('<nav class="links" id="menuheader" style="--items: 5;"><a class="font-weight-bolder " href="/Home">Runeterra BR</a><a href="/Cartas">Cartas</a><a href="/Decks">Decks</a><a href="#">Sla</a> <a href="#">Sobre</a><span class="line"></span></nav>').change();
+
+
+
+
+    }
+
+});
